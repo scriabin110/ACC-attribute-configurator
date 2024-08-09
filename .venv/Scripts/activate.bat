b8d7f9877bfd916acc35843b9add6295896a5c40
Ito Keisuke <ito.keisuke3@jgc.com>
2024-08-09 10:03:05 +0900

    こっちでも動くようにした

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\106-5180\OneDrive - JGC Group\デスクトップ\DPD(1)_BIM unit作業フォルダ\240807_ACC API token\.venv
+set VIRTUAL_ENV=C:\Users\106-5180\OneDrive - JGC Group\デスクトップ\ACC attribute configurator\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
