58a99d6893970ae390c089ea5c6bc23f72cbca8f
Ito Keisuke <ito.keisuke3@jgc.com>
2024-08-19 11:58:20 +0900

    no message

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
-from dotenv import load_dotenv
 
 # Streamlit Cloudで動作しているかどうかを確認
 is_streamlit_cloud = os.environ.get('STREAMLIT_CLOUD') == 'true'
 
 if is_streamlit_cloud:
+    from dotenv import load_dotenv
     # Streamlit Cloudで動作している場合の設定
 
     load_dotenv()
